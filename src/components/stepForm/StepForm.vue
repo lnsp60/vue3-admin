9ec7ab15b4c172778c8396a3d7ed6a4f6f400701
zhi <228568405@qq.com>
2026-03-05 11:21:33 +0800

    feat: 报警管理页面

---
+++ src/components/stepForm/StepForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <div style="max-width: 600px;">
+    <el-steps style="max-width: 600px" :active="currentStep" finish-status="success" align-center>
+      <el-step v-for="(step,index) in steps" :key="index" :title="step.title" />
+    </el-steps>
+    <div v-if="currentStep===0" class="mt">
+      <slot name="step-1"></slot>
+    </div>
+    <div v-if="currentStep===1" class="mt">
+      <slot name="step-2"></slot>
+    </div>
+    <div v-if="currentStep===2" class="mt">
+      <slot name="step-3"></slot>
+    </div>
+    <div class="step-buttons">
+      <el-button v-if="currentStep>0" @click="prevStep">上一步</el-button>
+      <el-button type="primary" @click="nextStep">
+        {{ currentStep!==steps.length-1?'下一步':'提交' }}
+      </el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const currentStep = ref<number>(0)
+const props = defineProps(["steps", "form1", "form2", "form3"])
+const emit = defineEmits(["handleSubmit"])
+
+const nextStep = () => {
+  const forms = [props.form1, props.form2, props.form3]
+  const currentForm = forms[currentStep.value]
+  currentForm.validate((valid:true) => {
+    if(valid){
+      if(currentStep.value < props.steps.length-1){
+        currentStep.value++
+      }else{
+        //让父组件去提交
+        emit("handleSubmit")
+      }
+    }
+  })
+}
+const prevStep = () => {
+  if(currentStep.value > 0){
+    currentStep.value--
+  }
+}
+</script>
--- src/views/alarm/Alarm.vue
+++ src/views/alarm/Alarm.vue
@@ -10,6 +10,8 @@
   <el-card class="mt" v-for="item in alarmList" :key="item.equNo">
     <el-alert :title="`${item.address}充电桩充电异常`" type="warning" show-icon />
     <el-descriptions :border="true" :column="4" direction="vertical" class="mt">
+      <!-- 遍历数组时，(item, index) in arr（第一个是元素，第二个是索引） -->
+      <!-- 遍历对象时，(val, key) 是「值在前、键在后」 -->
       <el-descriptions-item v-for="(val,key) in item" :label="getLabel(key)">
         <el-tag v-if="key==='level'" :type="val===1?'danger':val===2?'warning':'info'">
           {{ val===1?'严重':val===2?'紧急':'一般' }}
@@ -20,16 +22,97 @@
         <span v-else>{{ val }}</span>
       </el-descriptions-item>
       <el-descriptions-item label="操作">
-        <el-button :type="item.status===2?'warning':'primary'">{{ item.status===1?"指派":item.status===2?"催办":"查看" }}</el-button>
+        <el-button @click="drawer=true; CDtitle=item.equNo" :type="item.status===2?'warning':'primary'">{{ item.status===1?"指派":item.status===2?"催办":"查看" }}</el-button>
       </el-descriptions-item>
     </el-descriptions>
   </el-card>
+  <el-drawer
+    v-model="drawer"
+    title="报警任务指派"
+  >
+    <StepForm :steps="steps" :form1="form1" :form2="form2" :form3="form3" @handle-submit="handleSubmit">
+      <template #step-1>
+        <el-form :model="formData.basicInfo" :rules="basicRules" ref="form1">
+          <!-- prop:与规则相关 -->
+          <el-form-item label="姓名" prop="name">
+            <el-input v-model="formData.basicInfo.name"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="formData.basicInfo.email"></el-input>
+          </el-form-item>
+          <el-form-item label="电话" prop="tel">
+            <el-input v-model="formData.basicInfo.tel"></el-input>
+          </el-form-item>
+          <el-form-item label="工号" prop="no">
+            <el-input v-model="formData.basicInfo.no"></el-input>
+          </el-form-item>
+          <el-form-item label="是否加急">
+            <el-switch v-model="formData.basicInfo.urgent"></el-switch>
+          </el-form-item>
+          <el-form-item label="其他选项">
+            <el-checkbox-group v-model="formData.basicInfo.other">
+              <el-checkbox value="1">更换设备</el-checkbox>
+              <el-checkbox value="2">仅维修</el-checkbox>
+              <el-checkbox value="3">需拍照片</el-checkbox>
+              <el-checkbox value="4">需报备</el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+          <el-form-item label="其他信息">
+            <el-input type="textarea" v-model="formData.basicInfo.remarks"></el-input>
+          </el-form-item>
+        </el-form>
+      </template>
+      <template #step-2>
+        <el-form :model="formData.shenpi" :rules="shenpiRules" ref="form2">
+          <el-form-item label="审批部门" prop="a">
+            <el-select placeholder="请选择申办部门" v-model="formData.shenpi.a">
+              <el-option label="总裁办" value="1"></el-option>
+              <el-option label="运营部" value="2"></el-option>
+              <el-option label="维修部" value="3"></el-option>
+              <el-option label="市场部" value="4"></el-option>
+              <el-option label="财务部" value="5"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item  label="抄送部门" prop="b">
+            <el-select placeholder="请选择抄送部门" v-model="formData.shenpi.b">
+              <el-option label="总裁办" value="1"></el-option>
+              <el-option label="运营部" value="2"></el-option>
+              <el-option label="维修部" value="3"></el-option>
+              <el-option label="市场部" value="4"></el-option>
+              <el-option label="财务部" value="5"></el-option>
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </template>
+      <template #step-3>
+        <el-form :model="formData.pInfo" :rules="pInfoRules" ref="form3">
+          <el-form-item label="负责人姓名：" prop="person">
+              <el-input  v-model="formData.pInfo.person"/>
+            </el-form-item>
+            <el-form-item label="负责人电话：" prop="tel">
+              <el-input  v-model="formData.pInfo.tel"/>
+          </el-form-item>
+        </el-form>
+      </template>
+    </StepForm>
+    <el-result
+      icon="warning"
+      :title="`设备编号：${CDtitle}`"
+      sub-title="该任务已催促2次，请抓紧处理"
+    >
+      <template #extra>
+        <el-button type="primary" @click="drawer=false">我已知晓</el-button>
+      </template>
+    </el-result>
+  </el-drawer>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
 import { alarmListApi } from '@/api/alarm';
 import { getLabel } from './fieldLabelMap'
+import StepForm from '@/components/stepForm/StepForm.vue';
+import { ElMessage, type FormInstance } from 'element-plus';
 
 interface AlarmListType{
   description: string,
@@ -48,4 +131,81 @@ onMounted(async() => {
   const { data } = await alarmListApi()
   alarmList.value = data
 })
+
+// 
+const drawer = ref<boolean>(false)
+const steps = ref([
+  {title: "基本信息"},
+  {title: "审批信息"},
+  {title: "负责人信息"}
+])
+
+const formData = ref({
+  basicInfo:{
+    name:"",
+    email:"",
+    tel:"",
+    no:"",
+    urgent:true,
+    other: [],
+    remarks: ""
+  },
+  shenpi:{
+    a:"",
+    b:""
+  },
+  pInfo:{
+    person:"",
+    tel:""
+  }
+})
+const CDtitle = ref("")
+
+const basicRules = {
+  name:[
+    {required:true,message:"请输入姓名",trigger:"blur"}
+  ],
+  email:[
+    {required:true,message:"请输入邮箱",trigger:"blur"}
+  ],
+  tel:[
+    {required:true,message:"请输入电话",trigger:"blur"}
+  ],
+  no:[
+    {required:true,message:"请输入工号",trigger:"blur"}
+  ]
+}
+
+const shenpiRules={
+  a:[
+    {required:true,message:"不能为空",trigger:"blur"}
+  ],
+  b:[
+    {required:true,message:"不能为空",trigger:"blur"}
+  ],
+}
+
+const pInfoRules={
+  person:[
+    {required:true,message:"不能为空",trigger:"blur"}
+  ],
+  tel:[
+    {required:true,message:"不能为空",trigger:"blur"}
+  ],
+}
+
+const form1 = ref<FormInstance>()
+const form2 = ref<FormInstance>()
+const form3 = ref<FormInstance>()
+
+//提交表单
+const handleSubmit = () => {
+  console.log(formData.value)
+  ElMessage({
+    message: '指派成功',
+    type: 'success',
+  })
+  drawer.value = false
+}
+
 </script>
